1eee88f3032894bdd147f906c9f727ee0528d78b
Jaac219 <jaac219@gmail.com>
2023-10-07 23:54:44 -0500

    feat(api-routes): añade rutas para crear, actualizar y borrar mineros
    Agrega tres nuevas rutas en el controlador de mineros:
    - POST '/miner' para crear un minero.
    - PATCH '/miner/:id' para actualizar un minero por su ID.
    - DELETE '/miner/:id' para borar un minero por su ID
    
    La lógica correspondiente se encuentra implementada en el controlador de mineros.

--- api/src/controllers/minerController.js
+++ api/src/controllers/minerController.js
@@ -1,4 +1,5 @@
 const MinerModel = require('../models/Miner.js')
+const { v4: uuidv4 } = require('uuid')
 
 const getAll = async (req, res) => {
   try {
@@ -8,7 +9,7 @@ const getAll = async (req, res) => {
     res.status(200).json(miners)
   } catch (e) {
     console.error(e)
-    return e
+    res.status(500).send(e)
   }
 }
 
@@ -22,11 +23,75 @@ const getOne = async (req, res) => {
     res.status(200).json(miner)
   } catch (e) {
     console.error(e)
-    return e
+    res.status(500).send(e)
+  }
+}
+
+const create = async (req, res) => {
+  try {
+    const insert = {
+      firstName,
+      lastName,
+      typeId,
+      identification,
+      municipality
+    } = req.body
+
+    insert._id = uuidv4()
+
+    const miner = await new MinerModel(insert).save()
+    res.status(200).send(miner?._id || {})
+  } catch (e) {
+    console.error(e)
+    res.status(500).send(e)
+  }
+}
+
+const update = async (req, res) => {
+  try {
+    const { id } = req.params
+    if(!id) throw new Error('Id miner is required') 
+
+    const {
+      firstName,
+      lastName,
+      typeId,
+      identification,
+      municipality
+    } = req.body
+
+    const update = { $set: {} }
+
+    if(firstName && firstName.trim()) update.$set.firstName = firstName
+    if(lastName && lastName.trim()) update.$set.lastName = lastName
+    if(typeId && typeId.trim()) update.$set.typeId = typeId
+    if(identification) update.$set.identification = identification
+    if(municipality && municipality.trim()) update.$set.municipality = municipality
+
+    const miner = await MinerModel.findByIdAndUpdate(id, update)
+    res.status(200).send(miner?._id || {})
+  } catch (e) {
+    console.error(e)
+    res.status(500).send(e)
+  }
+}
+
+const deleteOne = async (req, res) => {
+  try {
+    const { id } = req.params
+    await MinerModel.deleteOne({ _id: id })
+
+    res.status(200).send(true)
+  } catch (e) {
+    console.error(e)
+    res.status(500).send(e)
   }
 }
 
 module.exports = {
   getAll,
-  getOne
+  getOne,
+  create,
+  update,
+  deleteOne
 }
--- api/src/routes/miner.js
+++ api/src/routes/miner.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const { getAll, getOne } = require('../controllers/minerController.js')
+const { getAll, getOne, deleteOne, create, update } = require('../controllers/minerController.js')
 
 router.get('/', getAll)
 router.get('/:id', getOne)
+router.post('/', create)
+router.patch('/:id', update)
+router.delete('/:id', deleteOne)
 
 module.exports = router
